c213e1f187551c421e21c50d80b219d1118bf416
treeshay <shayleighton24@gmail.com>
2025-09-24 14:29:08 +0100

    Question class improvements.
    Removed the old non class question method and replaced by a new question_test() method.
    
    Added getters, a display method and an answer check method to the question class.

--- src/Main.java
+++ src/Main.java
@@ -2,41 +2,6 @@ import java.util.Scanner;
 
 public class Main {
 
-    static void questions(Scanner input) {
-        String[] questions = {"What is the capital of France?", "What is the capital of Germany?", "What is the capital of Itlay?"};
-        String[] options = {"A| Paris", "B| London", "C| Berlin", "D| Rome"};
-        String[] correctAnswers = {"a", "c", "d"};
-        int score = 0;
-        for (int i = 0; i < questions.length; i++) {
-            System.out.println(questions[i]);
-            for (String option : options) {
-                System.out.println(option);
-            }
-            while (true) {
-                System.out.println("Please make choice [A/B/C/D]: ");
-                String answer = input.nextLine();
-                if (answer.equalsIgnoreCase(correctAnswers[i])) {
-                    System.out.println("Correct!");
-                    score += 10;
-                    System.out.println("Your score is: " + score);
-                    break;
-                } else {
-                    System.out.println("Wrong!");
-                    break;
-                }
-            }
-        }
-        System.out.println("Total score: " + score);
-    }
-
-    static void question_test(){
-        Question question_1 = new Question("Capital Of France","","A) Paris","B) London","C) Berlin","D) Rome" );
-        Question question_2 = new Question("Capital Of Spain","","A) Paris","B) Dubai","C) Madrid","D) Belfast");
-        Question question_3 = new Question("Capital Of Germany","","A) Paris","B) Berlin","C) London","D) Rome");
-
-        System.out.println(question_1.question,question_1.options,question_1.correctAnswer);
-    }
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         while (true) {
@@ -49,7 +14,7 @@ public class Main {
 
             if (choice == 1) {
                 System.out.println("Game is starting...");
-                question_test();
+                question_test(input);
             } else if (choice == 2) {
                 System.out.println("Goodbye!");
                 break;
@@ -59,4 +24,16 @@ public class Main {
         }
 
     }
+
+    static void question_test(Scanner input){
+        Question question_1 = new Question("Capital Of France","1","Paris","London","Berlin","Rome" );
+        question_1.displayQuestion();
+        while(true){
+            System.out.println("Please make choice [1/2/3/4]: ");
+            String answer = input.nextLine();
+            question_1.checkAnswer(answer);
+            break;
+            }
+        }
+
 }
--- src/Question.java
+++ src/Question.java
@@ -9,4 +9,33 @@ public class Question {
         this.options = options;
         this.correctAnswer = correctAnswer;
     }
+
+    //Getters
+    String getQuestion() {
+        return question;
+    }
+    String[] getOptions() {
+        return options;
+    }
+    String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    //Question Display
+    void displayQuestion(){
+        System.out.println(this.question);
+        System.out.println("Choose between 1 - " + options.length);
+        for (int i = 0; i < options.length; i++) {
+            System.out.println((i+1)+") "+options[i]);
+        }
+    }
+
+    //Answer check
+    void checkAnswer(String answer){
+        if(answer.equalsIgnoreCase(correctAnswer)){
+            System.out.println("Correct!");
+        }else{
+            System.out.println("Wrong!");
+        }
+    }
 }
